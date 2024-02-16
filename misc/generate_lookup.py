68822f5c93a2512144924aa0530587ab2b155a5a
Kyle Croarkin <kylesc4@illinois.edu>
2024-02-16 09:58:40 -0600

    Added Q-tail. Now need to add comma-tail and we're all good

--- misc/generate_lookup.py
+++ misc/generate_lookup.py
@@ -25,7 +25,7 @@ tbl = pd.read_excel("misc/lookup_table.xlsx")
 switch = []
 
 
-print("u8 lookup_table[" + str(tbl.shape[0]) + "][6] = {")
+print("u8 lookup_table[" + str(tbl.shape[0]) + "][7] = {")
 for i, row in tbl.iterrows(): 
     print("{")
     
@@ -40,13 +40,14 @@ for i, row in tbl.iterrows():
 
     q_hflip = (1 << 7) if row['q_hflip'] else 0 
 
-    print("{0}, {1}, {2}, {3}, {4}, {5}".format(
+    print("{0}, {1}, {2}, {3}, {4}, {5}, {6}".format(
         hex(row['Top']    | t_hflip | t_vflip),
         hex(row['Middle'] | m_hflip | m_vflip),
         hex(row['Bottom'] | b_hflip | b_vflip),
         hex(row['g_union']),
         hex(row['p_union']),
-        hex(row['q_union'] | q_hflip)
+        hex(row['q_union'] | q_hflip), 
+        hex(row['Q_union']) 
     ))
     print("},")
 
--- src/graphics/text.h
+++ src/graphics/text.h
@@ -2,16 +2,17 @@
 #define TEXT_H_
 #include <genesis.h>
 
-extern u8 lookup_table[74][6];
+extern u8 lookup_table[74][7];
 u8 *get_char_info(char c);
 
 #define GET_TOP(arr) (arr[0] & 0x3F)
 #define GET_TOP_VFLIP(arr) ((arr[0] >> 6) & 1)
 #define GET_TOP_HFLIP(arr) (arr[0] >> 7)
 
-#define GET_TOP_g_TAIL(arr) (arr[3] & 0x7F)
-#define GET_TOP_p_TAIL(arr) (arr[4] & 0x7F)
+#define GET_TOP_g_TAIL(arr) (arr[3])
+#define GET_TOP_p_TAIL(arr) (arr[4])
 #define GET_TOP_q_TAIL(arr) (arr[5] & 0x7F)
+#define GET_TOP_Q_TAIL(arr) (arr[6])
 
 #define GET_q_TAIL_HFLIP(arr) (arr[5] >> 7)
 
