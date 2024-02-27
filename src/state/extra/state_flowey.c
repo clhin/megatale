55ba2a8787e866be9ab4d4ee55926d93406aa442
Kyle Croarkin <kylesc4@illinois.edu>
2024-02-27 13:24:52 -0600

    Saving work

--- src/state/extra/state_flowey.c
+++ src/state/extra/state_flowey.c
@@ -1,9 +1,19 @@
 #include "state_flowey.h"
 
 #include <genesis.h>
+#include <resources.h>
 
 #include "../../graphics/textbox.h"
-void flowey_init(state_parameters_t args) { SPR_init(); }
+
+Sprite *heart;
+
+void flowey_init(state_parameters_t args) {
+    SPR_init();
+    box_draw(15, 14, 10, 9, PAL1);
+
+    heart = SPR_addSprite(&heart_sprite, 20 * 8 - 4, 19 * 8 - 4,
+                          TILE_ATTR(PAL1, TRUE, FALSE, FALSE));
+}
 void flowey_input(u16 changed, u16 state) {}
 void flowey_update() {}
 void flowey_clean() { VDP_clearTextArea(0, 0, 40, 28); }
